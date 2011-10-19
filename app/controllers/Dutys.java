3ba38db842ca7477d0b8ab3a30a23d1090eb36d8
Martin C. Richards <martinffx@gmail.com>
2011-10-19 22:03:36 +0200

    List override broken

--- app/controllers/Dutys.java
+++ app/controllers/Dutys.java
@@ -3,7 +3,16 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
+import models.*;
+
+import java.util.*;
+
 @Check("admin")
 @With(Secure.class)
 public class Dutys extends CRUD {
+
+    public static void list(String category){
+        List<Duty> object = Duty.getByCategory(category);
+        render(object);
+    }
 }
